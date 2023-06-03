0b08c1bb2d98ff7fcf637a36a51d266ca7fc6562
Bryan Miller <bmilcs@yahoo.com>
2023-06-03 19:38:56 -0400

    Fix base path for vite config/gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/interview-test-project",
+  base: "/interview-test-project/",
   plugins: [react()],
 });
