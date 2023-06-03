bb56e162c2367f42c90a25a12a69b064cd474291
Bryan Miller <bmilcs@yahoo.com>
2023-06-03 15:22:41 -0400

    Style hero on desktop

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import HeroImg from "../../images/hero.png";
+import Button from "../Button/Button";
 import "./Hero.css";
 
 function Hero() {
   return (
-    <section className="hero bg-light">
-      <div className="column-large">
-        <div className="hero-content">
+    <section className="bg-light">
+      <div className="column-large hero">
+        <div className="hero__content">
           <h2>The most intuitive online booking system</h2>
           <p>
             Calendio booking software helps you automate bookings, payments and
@@ -13,6 +14,15 @@ function Hero() {
             schedule and availability, and watch your appointments get booked
             24/7.
           </p>
+
+          <div className="hero__buttons">
+            <Button btnColor="dark" btnSize="large">
+              Start Free Trial
+            </Button>
+            <Button btnColor="darker" btnSize="large">
+              Schedule Demo
+            </Button>
+          </div>
         </div>
         <div className="hero-image-container">
           <img src={HeroImg} alt="Hero Image" />
